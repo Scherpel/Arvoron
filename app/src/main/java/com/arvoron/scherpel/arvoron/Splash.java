bf57c56d9f61a17f8a7aa9593d085b4efac6123a
scherpel <lucasguarnelli@hotmail.com>
2019-02-17 22:48:39 -0300

    Splash Screen added up!

---
+++ app/src/main/java/com/arvoron/scherpel/arvoron/Splash.java
@@ -0,0 +1,37 @@
+package com.arvoron.scherpel.arvoron;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    private ImageView fonte;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        fonte = (ImageView) findViewById(R.id.fonte);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.fadein);
+        fonte.startAnimation(myanim);
+
+        final Intent i = new Intent(this, MenuPrincipal.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
+
