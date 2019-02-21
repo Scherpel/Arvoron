47b822e650c475aa8972b4fd8a1dfb651a11c7a4
scherpel <lucasguarnelli@hotmail.com>
2019-02-21 20:08:23 -0300

    Added Firebase

---
+++ app/src/main/java/com/arvoron/scherpel/arvoron/Catalog.java
@@ -0,0 +1,13 @@
+package com.arvoron.scherpel.arvoron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Catalog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
--- app/src/main/java/com/arvoron/scherpel/arvoron/MenuPrincipal.java
+++ app/src/main/java/com/arvoron/scherpel/arvoron/MenuPrincipal.java
@@ -1,13 +1,41 @@
 package com.arvoron.scherpel.arvoron;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class MenuPrincipal extends AppCompatActivity {
-
+public class MenuPrincipal extends AppCompatActivity implements View.OnClickListener{
+    Intent i;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        CardView treeCatalog= findViewById(R.id.cadastro);
+        CardView treeList = findViewById(R.id.list);
+        CardView treeMap = findViewById(R.id.map);
+        CardView treeReport = findViewById(R.id.chat);
+        treeCatalog.setOnClickListener(this);
+        treeList.setOnClickListener(this);
+        treeMap.setOnClickListener(this);
+        treeReport.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.cadastro: i = new Intent(this, Catalog.class); startActivity(i); break;
+            case R.id.list: i = new Intent(this, Catalog.class); startActivity(i);break;
+            case R.id.map: i = new Intent(this, Catalog.class); startActivity(i);break;
+            case R.id.chat: i = new Intent(this, Catalog.class); startActivity(i);break;
+            default:break;
+        }
+
+
+
     }
 }
